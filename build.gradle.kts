0bdffef427f8ab54f93347800f5e2d20fad4d2c0
蜡笔小强 <89714521+Asyncload@users.noreply.github.com>
2026-04-25 00:53:14 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
-    }
+// 正确示例（根build.gradle.kts）
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
-    }
-}
+// 删掉了repositories块，不再在这里配置仓库
