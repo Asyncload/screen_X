7cf70b8c63fae90bb40cfa4154cbb0a9533be7d5
蜡笔小强 <89714521+Asyncload@users.noreply.github.com>
2026-04-25 00:43:15 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,20 @@
-// 在 settings.gradle.kts 的 dependencyResolutionManagement -> repositories 块中
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        // 修正 jitpack 仓库配置
-        maven {
-            url = uri("https://jitpack.io")
-        }
-        // 修正 xposed 仓库配置
-        maven {
-            url = uri("https://api.xposed.info/")
-        }
+        // 你的自定义仓库配置
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://api.xposed.info/") }
     }
 }
+
+rootProject.name = "你的项目名称"
+include(":app")
