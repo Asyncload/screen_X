5c1d52b2e0e3ce4604e16cd90373ef79589e026e
蜡笔小强 <89714521+Asyncload@users.noreply.github.com>
2026-04-25 21:37:39 +0800

    Update XposedModule.kt

--- app/src/main/java/com/lbxq/screen/XposedModule.kt
+++ app/src/main/java/com/lbxq/screen/XposedModule.kt
@@ -5,12 +5,8 @@ import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class XposedModule : IXposedHookLoadPackage {
+    // 不做任何包名判断，只要有应用启动就打印日志
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        // 测试：当支付宝启动时，打印日志
-        if (lpparam.packageName == "com.eg.android.AlipayGphone") {
-            XposedBridge.log("💰 ScreenModule支付宝测试成功！")
-            XposedBridge.log("当前包名：${lpparam.packageName}")
-            XposedBridge.log("进程名：${lpparam.processName}")
-        }
+        XposedBridge.log("✅模块已捕获应用：${lpparam.packageName}🎉")
     }
 }
